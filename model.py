496d8790a0507bd33fdeaca2f946ec79a2f0107d
Arseniy <arsfizteh@gmail.com>
2021-03-25 00:00:28 +0300

    Update model.py

--- model.py
+++ model.py
@@ -3,10 +3,11 @@ import torch
 
 class R_Net(torch.nn.Module):
 
-	def __init__(self, in_channels = 3, n_channels = 64, kernel_size = 5, std = 1):
+	def __init__(self, activation = torch.nn.SELU, , in_channels = 3, n_channels = 64, kernel_size = 5, std = 1):
 
 		super(R_Net, self).__init__()
 
+		self.activation = activation
 		self.in_channels = in_channels
 		self.n_c = n_channels
 		self.k_size = kernel_size
@@ -46,19 +47,19 @@ class R_Net(torch.nn.Module):
 
 	def encoder(self, x):
 
-		x = self.en_bn1(torch.nn.SELU(self.enconv1(x)))
-		x = self.en_bn2(torch.nn.SELU(self.enconv2(x)))
-		x = self.en_bn3(torch.nn.SELU(self.enconv3(x)))
-		z = self.en_bn4(torch.nn.SELU(self.enconv4(x)))
+		x = self.en_bn1(self.activation(self.enconv1(x)))
+		x = self.en_bn2(self.activation(self.enconv2(x)))
+		x = self.en_bn3(self.activation(self.enconv3(x)))
+		z = self.en_bn4(self.activation(self.enconv4(x)))
 
 		return z
 
 	def decoder(self, z):
 
-		z = torch.de_bn1(torch.nn.SELU(self.deconv1(x)))
-		z = torch.de_bn2(torch.nn.SELU(self.deconv2(x)))
-		z = torch.de_bn3(torch.nn.SELU(self.deconv3(x)))
-		x_out = torch.nn.SELU(self.deconv4(x))
+		z = torch.de_bn1(self.activation(self.deconv1(x)))
+		z = torch.de_bn2(self.activation(self.deconv2(x)))
+		z = torch.de_bn3(self.activation(self.deconv3(x)))
+		x_out = self.activation(self.deconv4(x))
 
 		return x_out
 
@@ -86,7 +87,7 @@ class D_Net(torch.nn.Module):
 		# Compute output dimension after conv part of D network
 		test_x = torch.Tensor(1, self.in_channels, self.in_resolution[0], self.in_resolution[1])
 		out = self.cnn(test_x)
-		self._out_dim =  torch.prod(torch.tensor(out.shape[1:])).item()
+		self.out_dim =  torch.prod(torch.tensor(out.shape[1:])).item()
 
 		self.fc1 = torch.nn.Linear(self.out_dim, 1024)
 		self.bn5 = torch.nn.BatchNorm1d(1024)
@@ -96,16 +97,31 @@ class D_Net(torch.nn.Module):
 
 		x = self.cnn(x)
 		x = torch.flatten(x, dim = 1)
-		x = self.bn5(.torch.nn.SELU(self.fc1(x)))
+		x = self.bn5(.self.activation(self.fc1(x)))
 		out = torch.nn.Sigmoid(self.fc2(x))
 
 		return out
 
 	def cnn(self, x):
 
-		x = self.bn1(torch.nn.SELU(self.conv1(x)))
-		x = self.bn2(torch.nn.SELU(self.conv2(x)))
-		x = self.bn3(torch.nn.SELU(self.conv3(x)))
-		x = self.bn4(torch.nn.SELU(self.conv4(x)))
+		x = self.bn1(self.activation(self.conv1(x)))
+		x = self.bn2(self.activation(self.conv2(x)))
+		x = self.bn3(self.activation(self.conv3(x)))
+		x = self.bn4(self.activation(self.conv4(x)))
 
 		return x
+
+
+def loss(y_real, y_fake, lambd = 0.4, R_loss = F.mse_loss):
+
+	r_loss = R_loss(x, r_x, reduction = 'mean')
+
+	ones = torch.ones(len(y_real), dtype = torch.long, device = device)
+	d_real_loss = F.cross_entropy(y_real, ones)
+
+	zeros = torch.zeros(len(y_real), dtype = torch.long, device = device)
+	d_fake_loss = F.cross_entropy(y_fakem zeros)
+
+	rd_loss = d_real_loss + d_fake_loss
+
+	return rd_loss + lambd * r_loss 
