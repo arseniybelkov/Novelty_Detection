47aa75893bfa1658556a0af509bd3fdb3dea3238
Arseniy <arsfizteh@gmail.com>
2021-03-29 23:33:21 +0300

    change model code

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ class R_Net(torch.nn.Module):
 		self.k_size = kernel_size
 		self.std = std
 
-		self.Encoder = torch.nn.ModuleList([torch.nn.Conv2d(self.in_channels, self.n_c, self.k_size, bias = False),
+		self.Encoder = torch.nn.Sequential(torch.nn.Conv2d(self.in_channels, self.n_c, self.k_size, bias = False),
 											torch.nn.BatchNorm2d(self.n_c),
 											self.activation(),
 											torch.nn.Conv2d(self.n_c, self.n_c*2, self.k_size, bias = False),
@@ -25,9 +25,9 @@ class R_Net(torch.nn.Module):
 											self.activation(),
 											torch.nn.Conv2d(self.n_c*4, self.n_c*8, self.k_size, bias = False),
 											torch.nn.BatchNorm2d(self.n_c*8),
-											self.activation()])
+											self.activation())
 
-		self.Decoder = torch.nn.ModuleList([torch.nn.ConvTranspose2d(self.n_c*8, self.n_c*4, self.k_size, bias = False),
+		self.Decoder = torch.nn.Sequential(torch.nn.ConvTranspose2d(self.n_c*8, self.n_c*4, self.k_size, bias = False),
 											torch.nn.BatchNorm2d(self.n_c*4),
 											self.activation(),
 											torch.nn.ConvTranspose2d(self.n_c*4, self.n_c*2, self.k_size, bias = False),
@@ -38,13 +38,13 @@ class R_Net(torch.nn.Module):
 											self.activation(),
 											torch.nn.ConvTranspose2d(self.n_c, self.in_channels, self.k_size, bias = False),
 											torch.nn.BatchNorm2d(self.in_channels),
-											self.activation()])
+											self.activation())
 
 	def forward(self, x):
 
 		x_hat = self.add_noise(x)
-		z = self.encoder(x_hat)
-		x_out = self.decoder(z)
+		z = self.Encoder(x_hat)
+		x_out = self.Decoder(z)
 
 		return x_out
 
@@ -54,24 +54,6 @@ class R_Net(torch.nn.Module):
 		x_hat = x + noise
 
 		return x_hat
-
-	def encoder(self, x):
-
-		z = x
-
-		for en_layer in self.Encoder:
-			z = en_layer(z)
-
-		return z
-
-	def decoder(self, z):
-
-		x_out = z
-
-		for de_layer in self.Decoder:
-			x_out = de_layer(x_out)
-
-		return x_out
 
 
 class D_Net(torch.nn.Module):
@@ -86,7 +68,7 @@ class D_Net(torch.nn.Module):
 		self.n_c = n_channels
 		self.k_size = kernel_size
 
-		self.cnn = torch.nn.ModuleList([torch.nn.Conv2d(self.in_channels, self.n_c, self.k_size, bias = False),
+		self.cnn = torch.nn.Sequential(torch.nn.Conv2d(self.in_channels, self.n_c, self.k_size, bias = False),
 										torch.nn.BatchNorm2d(self.n_c),
 										self.activation(),
 										torch.nn.Conv2d(self.n_c, self.n_c*2, self.k_size, bias = False),
@@ -97,23 +79,22 @@ class D_Net(torch.nn.Module):
 										self.activation(),
 										torch.nn.Conv2d(self.n_c*4, self.n_c*8, self.k_size, bias = False),
 										torch.nn.BatchNorm2d(self.n_c*8),
-										self.activation()])
+										self.activation())
 
 		# Compute output dimension after conv part of D network
 
 		self.out_dim = self._compute_out_dim()
 
-		self.fc = torch.nn.ModuleList([torch.nn.Linear(self.out_dim, 1024),
-										torch.nn.Sigmoid()])
+		self.fc = torch.nn.Sequential(torch.nn.Linear(self.out_dim, 1),
+										torch.nn.Sigmoid())
 
 	def _compute_out_dim(self):
 		
 		test_x = torch.Tensor(1, self.in_channels, self.in_resolution[0], self.in_resolution[1])
 		for p in self.cnn.parameters():
 			p.requires_grad = False
-		for cnn_layer in self.cnn:
-			test_x  = cnn_layer(test_x)
-		out_dim =  torch.prod(torch.tensor(test_x.shape[1:])).item()
+		test_x = self.cnn(test_x)
+		out_dim = torch.prod(torch.tensor(test_x.shape[1:])).item()
 		for p in self.cnn.parameters():
 			p.requires_grad = True
 
@@ -121,25 +102,22 @@ class D_Net(torch.nn.Module):
 
 	def forward(self, x):
 
-		for cnn_layer in self.cnn:
-			x = cnn_layer(x)
+		x = self.cnn(x)
 
-		out = torch.flatten(x, start_dim = 1)
+		x = torch.flatten(x, start_dim = 1)
 
-		for fc_layer in self.fc:
-			out = fc_layer(out)
+		out = self.fc(x)
 
 		return out
 
 
 def R_Loss(d_net: torch.nn.Module, x_real: torch.Tensor, x_fake: torch.Tensor, lambd: float) -> dict:
 
-	rec_loss = F.mse_loss(x_fake, x_real)
-
 	pred = d_net(x_fake)
 	y = torch.ones_like(pred)
 
-	gen_loss = F.binary_cross_entropy(pred, y) # generator loss
+	rec_loss = F.mse_loss(x_fake, x_real, reduction = 'sum')
+	gen_loss = F.binary_cross_entropy(pred, y, reduction = 'sum') # generator loss
 
 	L_r = gen_loss + lambd * rec_loss
 
@@ -154,7 +132,7 @@ def D_Loss(d_net: torch.nn.Module, x_real: torch.Tensor, x_fake: torch.Tensor) -
 	y_real = torch.ones_like(pred_real)
 	y_fake = torch.ones_like(pred_fake)
 
-	real_loss = F.binary_cross_entropy(pred_real, y_real)
-	fake_loss = F.binary_cross_entropy(pred_fake, y_fake)
+	real_loss = F.binary_cross_entropy(pred_real, y_real, reduction = 'sum')
+	fake_loss = F.binary_cross_entropy(pred_fake, y_fake, reduction = 'sum')
 
 	return real_loss + fake_loss
